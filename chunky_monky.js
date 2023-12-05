a61e9cf07dce0083f1761cbea02fdcc5180a8749
Jay Crawford <jayhcrawford@gmail.com>
2023-12-05 02:48:58 -0800

    First Impression

--- chunky_monky.js
+++ chunky_monky.js
@@ -11,5 +11,10 @@ function chunkArrayInGroups(arr, size) {
   chunkArrayInGroups(["a", "b", "c", "d"], 2);
 
 
-  //chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
-  //chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
+  //chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]]
+  //chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]]
+
+
+  //----------my initial instincts pointed me to this answer;
+  //------------The final array is of length 1. I think I will need a for loop.
+  //chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]]
