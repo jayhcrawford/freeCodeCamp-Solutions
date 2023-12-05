7a859f36af6105add4f01c344b85fad8afbaba43
Jay Crawford <jayhcrawford@gmail.com>
2023-12-05 15:03:30 -0800

    Just Kidding...
    I'm going to work on a second solution for this. I'm not understanding something about the function of these loops. For some reason, it's returning the array values that I set at the very beginning of the function... effectively doing nothing. I am going to attempt to translate this into a for loop, which I think that I understand more. See chunky_monky2.j

--- chunky_monky.js
+++ chunky_monky.js
@@ -1,33 +1,30 @@
-/*
-Write a function that splits an array (first argument) 
-into groups the length of size (second argument) and 
-returns them as a two-dimensional array.
-*/
-
 function chunkArrayInGroups(arr, size) {
     //initialize a new array to hold arrays that the for loop will create
     let newArr = [];
     let newArr2 = [];
+    const arrayLength = arr.length;
 
-    //I'm adding these variables to manipulate in the Do - While Loop.
+    //I just turned off Copilot completions because it was actually confusing me.
+    //This for loop will iterate through the array inputed into the function.
     let shifter = 0;
     let i = 0;
 
-
-    //I'm getting confused by the output of this function. I am going to take a break on it.
     do {
-        newArr2.push(arr[i]);
+        if (i !== arrayLength && newArr2.length !== 0) {
+          newArr2.push(arr[i]);
+        } 
         
+        shifter++;
 
         console.log("shifter: " + shifter + ", i: " + i);
-        if (shifter == size - 1) {
+        if (shifter == size) {
           newArr.push(newArr2);
           newArr2 = [];
           shifter = 0;
         }
-        console.log(newArr)
-        shifter++;
+        
         i++;
+        
     }   while (i <= arr.length);
     
     
@@ -35,14 +32,10 @@ function chunkArrayInGroups(arr, size) {
     return newArr;
   }
 
-  
-  chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+  //chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)
+  //console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
 
 
-  //chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]]
-  //chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]]
-
-
-  //----------my initial instincts pointed me to this answer;
-  //------------The final array is of length 1. I think I will need a for loop.
-  //chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]]
+console.log(chunkArrayInGroups(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)));
+console.log("This is the solution.")
