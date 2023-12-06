578a3c709dc613c422b6554f30da79fa41751fa7
Jay Crawford <jayhcrawford@gmail.com>
2023-12-05 16:57:44 -0800

    Palindrome
    I am going to start working on the final projects for the Javascript Certification at fCC. I think that this will help me to progress in my learning faster.

---
+++ palindrome.js
@@ -0,0 +1,24 @@
+//This is one of the final projects for the freeCodeCamp certification. I am going to start working on it. 
+//I want to show my process, and write as I am finding the solution.
+
+//Here is the prompt:
+/*
+Palindrome Checker
+Return true if the given string is a palindrome. Otherwise, return false.
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
+We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
+*/
+//(close) prompt
+
+
+//The examples that should pass are showing me that I will need to include special characters in my checking 
+//method.
+
+
+function palindrome(str) {
+    return true;
+  }
+  
+  palindrome("eye");
