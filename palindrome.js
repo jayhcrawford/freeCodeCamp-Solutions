456213150a1e3be5a832bf464cdb514b8e27d896
Jay Crawford <jayhcrawford@gmail.com>
2023-12-05 17:25:04 -0800

    Works on about 50% of Cases
    This palindrome checker is working on about 50% of the example cases. The cases that are testing false when they should be testing true are the cases which contain special characters. I will keep tinkering with this until they are accounted for.

--- palindrome.js
+++ palindrome.js
@@ -1,45 +1,49 @@
-/*
-
-This is one of the final projects for the freeCodeCamp certification. I am going to start working on it. 
-I want to show my process, and write as I am finding the solution.
-
-Here is the prompt:
-
-Palindrome Checker
-Return true if the given string is a palindrome. Otherwise, return false.
-A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
-Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
-We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
-We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
-(close) prompt
-*/
-
-
-/*
-The examples that should pass are showing me that I will need to include special characters in my checking 
-method.
-for example: 
-"palindrome("0_0 (: /-\ :) 0-0") should return true."
-/*
-
-/*
-My initial instinct is to create a new string from the inputted string, without any spaces. 
-Then flip that "no-spaces" string into another string. Then compare the two strings, and 
-return true if it's a palindrome. 
-*/
-
-
 function palindrome(str) {
-    let str1 = str;
+    let str1 = "";
     let str2 = "";
+    let regExp = /\s/;
+
+    //This will iterate through each character of the string argument "str" and then push it into a new string.
+    for (let i = 0; i<str.length; i++) {
+      
+    /*
+      This is really close to working. I am going to run tests to see what adjustments need to be made.
+      //I know for sure that it won't test correctly on one of the examples. This algorithim skips over '/'.
+      I am going to build a RegExp specifically for that character. 
+    */
+    let gg = regExp.test(str[i]);
+      
+    if (gg == false) {
+        str1 += str[i];
+    }
+      
+    }
+
+    /*
+    This will build the second string to test it against the first string.
+    */
+
+    for (let i = str1.length-1; i>=0; i--) {
+      str2 += str1[i]
+    }
   
-    
+    /*
+    This variable truthChecker will be the return value if the string argument "str" is a palindrome.
+    This if statement will check each character to test if the characters in either string are the same. If they are not, the "if" statement will return false statement and the function will end.
+    */
+    let truthChecker = true;
+    for (let i = 0; i<str1.length; i++) {
+      if (str1[i] !== str2[i]) {
+        return false;
+      }
+    }
+
   
   
-    return str2;
+    return truthChecker;
   }
   
-  palindrome("eye");
+ // palindrome("eye");
   
-  console.log(palindrome("0_0 (: /-\ :) 0-0"));
-
+  //console.log(palindrome("0_0 (: /-\ :) 0-0"));
+  palindrome("never odd or even")
