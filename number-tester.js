17ff2f4a8674f88585ba6a9249da24327df6e1c8
Jay Crawford <jayhcrawford@gmail.com>
2023-12-05 22:27:38 -0800

    Cash Register

--- number-tester.js
+++ number-tester.js
@@ -1,16 +1,31 @@
 
-//This is filtering out any characters from the input that are unrelated to the numerical characters.
+//So this is passing 90% of the test cases, but the cases that are failing are all weird, and I'm gonna have to get nitty gritty, just for them.
+//For now I want to take a break.
 function telephoneCheck(str) {
     let strSansNonNumeric = "";
-    let nonNumericTester = /-()/;
+    let nonNumericTester = /[-!*&#a-zA-Z()]/;
   
     for (let i = 0; i<str.length ; i++) {
       if (nonNumericTester.test(str[i]) == false) {
+        if (str[i] == ' ') {
+          continue;
+        } else {
         strSansNonNumeric += str[i];
+        }
       }
     }
     console.log(strSansNonNumeric);
-    return true;
+
+    if (strSansNonNumeric.length == 10) {
+      return true;
+    }
+    if (strSansNonNumeric.length == 11) {
+      if (strSansNonNumeric[0] !== '1') {
+        return false;
+      } else {
+        return true;
+      }
+    }
+    return false;
   }
   
-  telephoneCheck("555-555-5555");
