c9d3df506dc20ea5260ab0f2468d9f147a1f3530
Jay Crawford <jayhcrawford@gmail.com>
2023-12-05 18:45:02 -0800

    Taking Care Of The Big Stuff First

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -1,32 +1,18 @@
-/*
-Okay, so I know for sure that ChatGPT can create a roman numeral converter in five seconds or less.
-
-I am still going to go about this the "Old Fashioned" way and actually complete it using my own brain.
-
-Here is the prompt: 
-
-Roman Numeral Converter
-Convert the given number into a roman numeral.
-
-Roman numerals	Arabic numerals
-M	1000
-CM	900
-D	500
-CD	400
-C	100
-XC	90
-L	50
-XL	40
-X	10
-IX	9
-V	5
-IV	4
-I	1
-All roman numerals answers should be provided in upper-case.
-*/
+//So this first pass is helping me to understand how simply this can be done. I think that it will be a straightforward task.
 
 function convertToRoman(num) {
-    return num;
+    let romNumStr = "";
+    let numTester = 0;
+
+    for (numTester = num; numTester>=1000;) {
+      romNumStr += 'M';
+      numTester -= 1000;
+    }
+
+    return romNumStr;
    }
    
-   convertToRoman(36);
+   console.log(convertToRoman(36));
+   console.log(convertToRoman(3999));
+   console.log(convertToRoman(1004));
+   console.log(convertToRoman(798));
