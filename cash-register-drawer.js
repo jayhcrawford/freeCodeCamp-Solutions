977349333db3c3eb19411a284ba7213d3690b95d
Jay Crawford <jayhcrawford@gmail.com>
2023-12-05 22:35:04 -0800

    Initial Stages

--- cash-register-drawer.js
+++ cash-register-drawer.js
@@ -1,44 +1,24 @@
-/*
-
-
-Here is the prompt: 
-Cash Register
-Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument 
-(price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
-
-cid is a 2D array listing available currency.
-The checkCashRegister() function should always return an object with a status key and a change key.
-Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you 
-cannot return the exact change.
-Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal 
-to the change due.
-Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest 
-to lowest order, as the value of the change key.
-
-Currency Unit	Amount
-Penny	$0.01 (PENNY)
-Nickel	$0.05 (NICKEL)
-Dime	$0.1 (DIME)
-Quarter	$0.25 (QUARTER)
-Dollar	$1 (ONE)
-Five Dollars	$5 (FIVE)
-Ten Dollars	$10 (TEN)
-Twenty Dollars	$20 (TWENTY)
-One-hundred Dollars	$100 (ONE HUNDRED)
-See below for an example of a cash-in-drawer array:
-(close prompt)
-
-
-
-This one is more complicated than many of the others. I think that I should be able to knock it out, 
-because the only increase in complication is the presence of objects.
-*/
-
-
+//First I'm creating a nested function that will be able to calculate the balance of the cash on hand.
+//It will calculate it before and after any transaction, and return the appropriate sum when necessary.
 
 function checkCashRegister(price, cash, cid) {
-    let change;
+    let change = cid;
+    let totalCashOnHand = 0;
+  
+  function checkBalance(cidArray) {
+    let cashOnHand = cidArray;
+    for (let i = 0; i<cashOnHand.length; i++) {
+      for (let j = 0; j<cashOnHand[i].length; j++) {
+        console.log(cashOnHand[i][j]);
+      }
+    }
+    return cashOnHand
+  }
+    checkBalance(change);
+  
     return change;
   }
   
   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  
+  //console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
