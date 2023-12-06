cf23770055f6d69bea3b0a072e092025b0b116c3
Jay Crawford <jayhcrawford@gmail.com>
2023-12-05 18:30:04 -0800

    Completed Palindrome
    This was a simpler project. I felt like I would be able to complete it in a short amount of time. Everything that this required was relatively basic. I am beginning to understand some of the basics! Yippee!!

--- palindrome.js
+++ palindrome.js
@@ -1,39 +1,80 @@
-/*
-This is taking care of every single case with the exception of one case. The thing that is preventing it from
-testing "true" on that one case is the fact that it is "flipping" the '(' in the final phase of the test. I 
-am going to create a special case for parenthesis that will take care if this.
-*/
+//Here is the solution, and a walk through.
 
 function palindrome(str) {
+    //These two strings will hold the string information that will be used to check if the str argument is a palindrome.
     let str1 = "";
     let str2 = "";
+
+    
+    //These RegExps will be used to test conditional statements.
+    //This first one will test for spaces.
     let regExp = /\s/;
+    //This one will test for any characters that are non-special character punctuation. I created this 
+    //specifically for the comma in one of the palindromes.
     let regExpPunct = /[^\w\s]+/;
+    //This RegExp will test for special characters that are not punctuation that are being used in the test cases.
     let regExpChar = /[\/():]+/;
     
+
+    /*
+    This for loop will iterate through every character in the string.
+    
+    It will test against the RegExps and add appropriate characters to the first local string "str1".
+    */
     for (let i = 0; i<str.length; i++) {
     
-    let spaces = regExp.test(str[i]);
-    let alphaNumSomeChars = regExpPunct.test(str[i]);
-    let specialChars = regExpChar.test(str[i]);
+        //These three local variables will be booleans that will determine whether or not the tested characters are added to the local string "str1".
+        let spaces = regExp.test(str[i]);
+        let alphaNumSomeChars = regExpPunct.test(str[i]);
+        let specialChars = regExpChar.test(str[i]);
     
-    if (specialChars == true) {
+
+        //These are the conditional statements that will append each character to the local string "str1".
+        if (specialChars == true) {
           str1 += str[i]
         }
 
-      if (alphaNumSomeChars == false){
-        if (str[i] !== '_') {
-          if (spaces == false) {
-          str1 += str[i].toLowerCase();
+        if (alphaNumSomeChars == false){
+            if (str[i] !== '_') {
+                if (spaces == false) {
+                    str1 += str[i].toLowerCase();
+                }
+            }
         }
-        }
-      }
     }
 
+
+    /*
+    This is where the second local string will be populated. I adjusted some of the parameters in this "For" 
+    statement to work specifically for test cases in this exercise. 
+
+    The final issue that I encountered with special characters was the parentheses cases--in order to make
+    the test case which had parentheses into a palindrome, I had to create a special "If" statement that 
+    would skip the str1[i] index and instead place in the opposite and appopriate parenthesis mark. 
+    */
     for (let i = str1.length-1; i>=0; i--) {
-      str2 += str1[i]
+      if (str1[i] == '(') {
+        str2 += ')'
+      }
+      else if (str1[i] == ')') {
+        str2 += '('
+      }
+      else {
+        str2 += str1[i]
+      }
       }
   
+
+    /*
+    This truthChecker will be the value that gets returned true, unless the following "If" statement is satisfied.
+    
+    The collowing "For" statement will iterate through each character of both local strings "str1" and "str2".
+    If the encoled "If" statement finds that either of the characters at their respective index [i] are not the same 
+    then the "If" statement will return false and the function will conclude, and return a false value.
+
+    If that enclosed "If" statement does not return false, then the function will return truthChecker
+    and the function will tell us that the string argument "str" is in fact, a palindrome.
+    */
     let truthChecker = true;
     for (let i = 0; i<str1.length; i++) {
       if (str1[i] !== str2[i]) {
@@ -45,4 +86,3 @@ function palindrome(str) {
   }
   
 console.log(palindrome("0_0 (: /-\ :) 0-0"));
-console.log(palindrome("_eye"));
